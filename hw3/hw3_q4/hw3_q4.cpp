825fab42c764393cc4ecd09b7f70a40f8cb3871c
github username <lhon001@gmail.com>
2020-01-27 01:58:53 -0500

    typecasting and rounding

---
+++ hw3/hw3_q4/hw3_q4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int FLOOR_ROUND = 1;
+    const int CEILING_ROUND = 2;
+    const int ROUND = 3;
+    double num;
+    int method;
+
+    cout << "Please enter a Real number: ";
+    cin >> num;
+
+    cout << "Choose your rounding method: " << endl;
+    cout << "1. Floor round" << endl;
+    cout << "2. Ceiling round" << endl;
+    cout << "3. Round to the nearest whole number" << endl;
+    cin >> method;
+
+    switch (method) {
+        case FLOOR_ROUND:
+            cout << (int)num;
+            break;
+        case CEILING_ROUND:
+            num +=1;
+            cout << (int)num;
+            break;
+        case ROUND:
+            num += 0.5;
+            cout << (int)num;
+            break;
+        default:
+            break;
+    }
+    
+    return 0;
+}
