a322bc4e4319ee4484a7c60b1df7515acb1876a5
github username <lhon001@gmail.com>
2020-02-03 23:13:29 -0500

    rename variables

--- hw4/hw4_q6/hw4_q6.cpp
+++ hw4/hw4_q6/hw4_q6.cpp
@@ -8,31 +8,31 @@ int main() {
     cin >> n;
 
     for (int i = 1; i <= n; i++) {
-        int number_of_digits = 0;
+        int number_of_current_digits = 0;
         int count = i;
         while (count != 0) {
             count = count / 10;
-            number_of_digits++;
+            number_of_current_digits++;
         }
 
         int factor = 1;
-        for (int b = 1; b < number_of_digits; b++) {
+        for (int b = 1; b < number_of_current_digits; b++) {
             factor *= 10;
         }
 
         int current_num = i;
-        int digit, even = 0, odd = 0;
-        for (int c = 0; c < number_of_digits; c++) {
-            digit = current_num / factor;
+        int current_digit, even_count = 0, odd_count = 0;
+        for (int c = 0; c < number_of_current_digits; c++) {
+            current_digit = current_num / factor;
             current_num %= factor;
-            if ((digit % 2) == 0) {
-                even++;
+            if ((current_digit % 2) == 0) {
+                even_count++;
             } else {
-                odd++;
+                odd_count++;
             }
             factor /= 10;
         }
-       if (even > odd) {
+       if (even_count > odd_count) {
            cout << i << endl;
        }
     }
