ac68544ed5cc9a6b4ff429c6efa808ca561ab210
github username <lhon001@gmail.com>
2020-02-07 23:30:29 -0500

    multiplication table

---
+++ hw5/hw5_q1/cmake-build-debug/CMakeFiles/hw5_q1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw5_q1.dir/hw5_q1.cpp.o"
+  "hw5_q1"
+  "hw5_q1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw5_q1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
