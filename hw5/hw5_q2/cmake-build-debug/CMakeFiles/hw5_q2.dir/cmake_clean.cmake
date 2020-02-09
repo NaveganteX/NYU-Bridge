3bd490692585a21edd002731225718dac65ad054
github username <lhon001@gmail.com>
2020-02-09 01:10:50 -0500

    random number guessing

---
+++ hw5/hw5_q2/cmake-build-debug/CMakeFiles/hw5_q2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw5_q2.dir/hw5_q2.cpp.o"
+  "hw5_q2"
+  "hw5_q2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw5_q2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
