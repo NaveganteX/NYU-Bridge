2ede953d19f93138a4389c9d889cc97aadc66b7b
github username <lhon001@gmail.com>
2020-02-15 18:26:55 -0500

    isPrime function

---
+++ practice/isPrime/isPrime.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n, divisor_Count;
+    cout << "Input a number: ";
+    cin >> n;
+
+    divisor_Count = 0;
+    for (int i = 2; i < sqrt(n); i++) {
+        if ((n % i) == 0) {
+            divisor_Count++;
+        }
+    }
+
+    if (divisor_Count == 0) {
+        cout << n << " is a prime number" << endl;
+    } else {
+        cout << n << " is not a prime number" << endl;
+    }
+
+    return 0;
+}
