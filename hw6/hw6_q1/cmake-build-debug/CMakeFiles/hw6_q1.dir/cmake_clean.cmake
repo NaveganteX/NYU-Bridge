711f5db1a8a55026a1f8efc13920244dcd0c205b
github username <lhon001@gmail.com>
2020-02-16 23:57:48 -0500

    nth fibonacci number

---
+++ hw6/hw6_q1/cmake-build-debug/CMakeFiles/hw6_q1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw6_q1.dir/hw6_q1.cpp.o"
+  "hw6_q1"
+  "hw6_q1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw6_q1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
