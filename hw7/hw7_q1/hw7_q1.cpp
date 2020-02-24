9b5b4f134a0ab2dce15d9ad9312cd48b7efa66a3
github username <lhon001@gmail.com>
2020-02-24 00:48:03 -0500

    rename variables for formatting consistency

--- hw7/hw7_q1/hw7_q1.cpp
+++ hw7/hw7_q1/hw7_q1.cpp
@@ -7,15 +7,15 @@ bool isLeapYear(int year);
 int printYearCalendar(int year, int startingDay);
 
 int main() {
-    int year = 0, first_day = 0;
+    int year = 0, firstDay = 0;
     cout << "Please enter a year: ";
     cin >> year;
 
     cout << "Enter the first day of the month (1-Mon, 2-Tues): ";
-    cin >> first_day;
+    cin >> firstDay;
     cout << endl;
 
-    printYearCalendar(year, first_day);
+    printYearCalendar(year, firstDay);
     return 0;
 }
 
@@ -44,57 +44,57 @@ int printMonthCalendar(int numOfDays, int startingDay) {
 }
 
 int printYearCalendar(int year, int startingDay) {
-    int days_in_month = 0, offSet = 0;
+    int daysInMonth = 0, offSet = 0;
     string month;
 
     for (int i = 1; i <= 12; i++) {
         if (i == 1) {
             month = "January";
-            days_in_month = 31;
+            daysInMonth = 31;
         } else if (i == 2) {
             month = "February";
             if (isLeapYear(year)) {
-                days_in_month = 29;
+                daysInMonth = 29;
             } else {
-                days_in_month = 28;
+                daysInMonth = 28;
             }
         } else if (i == 3) {
             month = "March";
-            days_in_month = 31;
+            daysInMonth = 31;
         } else if (i == 4) {
             month = "April";
-            days_in_month = 30;
+            daysInMonth = 30;
         } else if (i == 5) {
             month = "May";
-            days_in_month = 31;
+            daysInMonth = 31;
         } else if (i == 6) {
             month = "June";
-            days_in_month = 30;
+            daysInMonth = 30;
         } else if (i == 7) {
             month = "July";
-            days_in_month = 31;
+            daysInMonth = 31;
         } else if (i == 8) {
             month = "August";
-            days_in_month = 31;
+            daysInMonth = 31;
         } else if (i == 9) {
             month = "September";
-            days_in_month = 30;
+            daysInMonth = 30;
         } else if (i == 10) {
             month = "October";
-            days_in_month = 31;
+            daysInMonth = 31;
         } else if (i == 11) {
             month = "November";
-            days_in_month = 30;
+            daysInMonth = 30;
         } else if (i == 12) {
             month = "December";
-            days_in_month = 31;
+            daysInMonth = 31;
         }
 
         cout << month << " " << year << endl;
         if (i == 1) {
-            offSet = printMonthCalendar(days_in_month, startingDay);
+            offSet = printMonthCalendar(daysInMonth, startingDay);
         } else {
-            offSet = printMonthCalendar(days_in_month, offSet);
+            offSet = printMonthCalendar(daysInMonth, offSet);
         }
         cout << endl;
         cout << endl;
