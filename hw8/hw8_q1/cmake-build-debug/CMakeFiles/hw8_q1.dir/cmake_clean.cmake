c4ba551ae97f433e05a23a6056ba109ada771075
github username <lhon001@gmail.com>
2020-02-29 19:35:12 -0500

    find smallest number in array

---
+++ hw8/hw8_q1/cmake-build-debug/CMakeFiles/hw8_q1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw8_q1.dir/hw8_q1.cpp.o"
+  "hw8_q1"
+  "hw8_q1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw8_q1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
