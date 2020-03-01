6f2fbc51403c9e330badd8bb8b5fb243540bf793
github username <lhon001@gmail.com>
2020-03-01 00:43:04 -0500

    remove odd numbers from array

---
+++ hw8/hw8_q3/cmake-build-debug/CMakeFiles/hw8_q3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw8_q3.dir/hw8_q3.cpp.o"
+  "hw8_q3"
+  "hw8_q3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw8_q3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
