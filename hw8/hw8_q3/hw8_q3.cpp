fde48186ae6124d6f6437ab3737bb2d3566c848c
github username <lhon001@gmail.com>
2020-03-02 23:04:41 -0500

    splitParity logic

--- hw8/hw8_q3/hw8_q3.cpp
+++ hw8/hw8_q3/hw8_q3.cpp
@@ -4,27 +4,28 @@ using namespace std;
 void printArray(int arr[], int arrSize);
 void reverseArray(int arr[], int arrSize);
 void removeOdd(int arr[], int& arrSize);
+void splitParity(int arr[], int arrSize);
 
 int main() {
     int arr1[10] = {9, 2, 14, 12, -3};
     int arr1Size = 5;
 
     int arr2[10] = {21, 12, 6, 7, 14};
-//    int arr2[10] = {11, 3, 6, 7, 14};
-//    int arr2[10] = {21, 11, 6, 7, 14};
     int arr2Size = 5;
 
-    int arr3[10] = {3, 6, 4, 1, 12};
+//    int arr3[10] = {3, 6, 4, 1, 12};
+//    int arr3[10] = {6, 8, 12, 11, 9};
+    int arr3[10] = {13, 7, 12, 11, 9};
     int arr3Size = 5;
 
 //    reverseArray(arr1, arr1Size);
 //    printArray(arr1, arr1Size);
 
-    removeOdd(arr2, arr2Size);
-    printArray(arr2, arr2Size);
+//    removeOdd(arr2, arr2Size);
+//    printArray(arr2, arr2Size);
 
-//    splitParity(arr3, arr3Size);
-//    printArray(arr3, arr3Size);
+    splitParity(arr3, arr3Size);
+    printArray(arr3, arr3Size);
 
     return 0;
 }
@@ -47,19 +48,28 @@ void reverseArray(int arr[], int arrSize) {
 }
 
 void removeOdd(int arr[], int& arrSize) {
-    int oddCount = 0, lowestOddNumIndex = 0, tempHolder = 0;
-    for (int i = 0; i < arrSize; i++) {
-        cout << "i: " << i << endl;
-        if (((arr[i] % 2) != 0)) {
-            lowestOddNumIndex = i;
-        } else {
-            tempHolder = arr[lowestOddNumIndex];
-            arr[lowestOddNumIndex] = arr[i];
+    int oddCount = 0, tempHolder = 0;
+    for (int i = 0, lowestOddIndex = 0; i < arrSize; i++) {
+        if (arr[i] % 2 == 0) {
+            tempHolder = arr[lowestOddIndex];
+            arr[lowestOddIndex] = arr[i];
             arr[i] = tempHolder;
-            lowestOddNumIndex = i;
-            cout << "lowestOddNumIndex: " << lowestOddNumIndex << endl;
+            lowestOddIndex += 1;
+        } else {
+            oddCount += 1;
         }
-        cout << endl;
     }
-//    arrSize -= oddCount;
+    arrSize -= oddCount;
+}
+
+void splitParity(int arr[], int arrSize) {
+    int tempHolder = 0;
+    for (int i = 0, lowestOddIndex = 0; i < arrSize; i++) {
+        if (arr[i] % 2 != 0) {
+            tempHolder = arr[lowestOddIndex];
+            arr[lowestOddIndex] = arr[i];
+            arr[i] = tempHolder;
+            lowestOddIndex += 1;
+        }
+    }
 }
