d21a5ead344463d4fca0b627c3f4a04826fabdb4
github username <lhon001@gmail.com>
2020-03-05 18:49:40 -0500

    update logic to print out correct PIN statement

--- hw8/hw8_q4/hw8_q4.cpp
+++ hw8/hw8_q4/hw8_q4.cpp
@@ -3,18 +3,21 @@
 #include <ctime>
 using namespace std;
 
-bool isPin(int arrNum[], int userInput[]);
-
-const int pin[5] = {3, 5, 6, 9, 1};
+const int PIN[5] = {3, 5, 6, 9, 1};
 
 int main() {
     int arrNum[5], userInput[5];
     int num = 0, digit = 0;
 
     cout << "Please enter your PIN according to the following mapping:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << PIN[i];
+    }
+    cout << endl;
     cout << "PIN: 0 1 2 3 4 5 6 7 8 9" << endl;
     cout << "NUM: ";
 
+//    generate NUM array with random numbers
     srand(time(0));
     for (int i = 0; i < 10; i++) {
         arrNum[i] = (rand() % 10);
@@ -31,22 +34,13 @@ int main() {
         num -= digit;
     }
 
-//    for (int i = 0; i < 5; i++) {
-//        cout << userInput[i] << endl;
-//    }
-    cout << endl;
-
-    isPin(arrNum, userInput);
-
+    for (int i = 0; i < 5; i++) {
+        if (arrNum[PIN[i]] != userInput[i]) {
+            i = 5;
+            cout << "Your PIN is not correct" << endl;
+        } else if ((i == 4) && (arrNum[PIN[i]] == userInput[i])) {
+            cout << "Your PIN is correct" << endl;
+        }
+    }
     return 0;
 }
-
-bool isPin (int arrNum[], int userInput[]) {
-//    cout << arrNum[0] << endl;
-//    cout << userInput[0] << endl;
-
-//    for (int i = 0; i < 5; i++) {
-//        if (userInput[i] == userInput)
-//    }
-}
-
