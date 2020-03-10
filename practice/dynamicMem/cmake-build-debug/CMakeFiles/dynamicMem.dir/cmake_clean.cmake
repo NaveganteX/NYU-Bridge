c01c02e5c8f1ae559b089232f13c3a97fd5a182b
github username <lhon001@gmail.com>
2020-03-10 03:12:38 -0400

    rename project and update CMake file

---
+++ practice/dynamicMem/cmake-build-debug/CMakeFiles/dynamicMem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dynamicMem.dir/dynamicMemory.cpp.o"
+  "dynamicMem"
+  "dynamicMem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dynamicMem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
