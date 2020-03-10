fe384132cd9833a7d4141d5574977f792f6b20cf
github username <lhon001@gmail.com>
2020-03-10 03:04:44 -0400

    add letter count function

---
+++ hw9/hw9_q1/cmake-build-debug/CMakeFiles/hw9_q1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw9_q1.dir/hw9_q1.cpp.o"
+  "hw9_q1"
+  "hw9_q1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw9_q1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
