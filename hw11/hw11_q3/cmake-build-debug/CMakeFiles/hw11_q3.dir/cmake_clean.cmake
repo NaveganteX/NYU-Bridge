c8b1cd441f48a13035ce9156f853f0467ef0893d
github username <lhon001@gmail.com>
2020-03-27 00:59:47 -0400

    minInArray1, minInArray2

---
+++ hw11/hw11_q3/cmake-build-debug/CMakeFiles/hw11_q3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw11_q3.dir/hw11_q3.cpp.o"
+  "hw11_q3"
+  "hw11_q3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw11_q3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
