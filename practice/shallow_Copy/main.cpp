acd7409a739702258f02b227ac31f940ac381902
github username <lhon001@gmail.com>
2020-04-02 01:12:23 -0400

    additional shallow copy notes

--- practice/shallow_Copy/main.cpp
+++ practice/shallow_Copy/main.cpp
@@ -17,10 +17,11 @@ int main() {
     Thing one(13);
     Thing two(28);
 
-/* Below, we are copying one to two. What is really happening is we are copying the pointer 'one'
- * to the value of object "two".
+/* Below, we are copying two into one ('=' is technically a copy operation).
+ * What is really happening is we are copying the pointer 'one' to the value of object "two", not the data.
  * Now, both objects one and two have the value 2, and the memory of where one was pointing to (with value 1)
- * will be a memory leak
+ * will be a memory leak. When main() ends, the memory created on the heap (original value of one) is
+ * never deleted.
  */
     cout << "Before shallow copy:" << endl;
     cout << one.getValue() << endl; // prints "13"
@@ -33,5 +34,8 @@ int main() {
     // one's *value now points to 28; no longer able to access original value 13 (memory leak)
     cout << one.getValue() << endl;
     cout << two.getValue() << endl;
+
+
+
     return 0;
 }
