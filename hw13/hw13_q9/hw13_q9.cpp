726f39be3393ba954bd6f5af0ea4704540805406
github username <lhon001@gmail.com>
2020-04-16 00:51:40 -0400

    implement checkPositions and move functionality

--- hw13/hw13_q9/hw13_q9.cpp
+++ hw13/hw13_q9/hw13_q9.cpp
@@ -4,29 +4,6 @@
 #include <vector>
 using namespace std;
 
-class Board;
-
-class Organism {
-    int x, y;
-public:
-    Organism(int x_coord, int y_coord) {
-        x = x_coord;
-        y = y_coord;
-    }
-    virtual void move() const = 0;
-    int getXCoord() { return x; }
-    int getYCoord() { return y; }
-
-};
-
-class Ant :public Organism {
-public:
-    Ant(int x_coord, int y_coord) :Organism(x_coord, y_coord) {}
-    void move() {
-
-    }
-};
-
 class Board {
     char board[20][20];
 public:
@@ -38,45 +15,156 @@ public:
         }
     }
 
-    void printBoard() {
-        for (int i = 0; i < 20; i++) {
-            cout << "  " << i;
-        }
-        cout << endl;
-        for (int y = 0; y < 20; y++) {
-            cout << y;
-            for (int x = 0; x < 20; x++) {
-                cout << " " << board[y][x] << " ";
-            }
-            cout << endl;
-        }
-    }
+//    void printBoard() {
+//        for (int i = 0; i < 20; i++) {
+//            cout << "  " << i;
+//        }
+//        cout << endl;
+//        for (int y = 0; y < 20; y++) {
+//            cout << y;
+//            for (int x = 0; x < 20; x++) {
+//                cout << " " << board[y][x] << " ";
+//            }
+//            cout << endl;
+//        }
+//    }
 
-    void updateBoard(vector<Ant> ants) {
-        int i, x, y;
-        for (i = 0; i < 10; i++) {
-            x = ants[i].getXCoord();
-            y = ants[i].getYCoord();
-            board[y][x] = 'o';
-        }
-    }
+//    void updateBoard(vector<Ant> ants) {
+//        int i, x, y;
+//        for (i = 0; i < 10; i++) {
+//            x = ants[i].getXCoord();
+//            y = ants[i].getYCoord();
+//            board[y][x] = 'o';
+//        }
+//    }
 
-    bool isCellAvailable(int x_coord, int y_coord) {
-        if (this->getCellStatus(x_coord, y_coord) == '-') {
-            return true;
-        }
+//    bool isCellAvailable(int x_coord, int y_coord) {
+//        if (this->getCellStatus(x_coord, y_coord) == '-') {
+//            return true;
+//        }
+//        return false;
+//    }
+
+//    char getCellStatus(int x_coord, int y_coord) {
+//        return board[y_coord][x_coord];
+//    }
+};
+
+class Organism {
+    int x, y;
+public:
+    Organism(int x_coord, int y_coord) {
+        x = x_coord;
+        y = y_coord;
+    }
+    virtual void move(vector<int> position_coords) {};
+    int getXCoord() { return x; }
+    int getYCoord() { return y; }
+    void setXCoord(int x_coord) { x = x_coord; }
+    void setYCoord(int y_coord) { y = y_coord; }
+};
+
+class Ant :public Organism {
+public:
+    Ant(int x_coord, int y_coord) :Organism(x_coord, y_coord) {}
+
+    void move(vector<int> position_coords) override {
+        this->setXCoord(position_coords[0]);
+        this->setYCoord(position_coords[1]);
+        Organism::move(position_coords);
+    }
+};
+
+bool isCellAvailable(char board[][20], int x_coord, int y_coord) {
+    if ((x_coord > 19) || (x_coord < 0) || (y_coord > 19) || (y_coord < 0)) {
         return false;
     }
 
-    char getCellStatus(int x_coord, int y_coord) {
-        return board[y_coord][x_coord];
+    if (board[y_coord][x_coord] == '-') {
+        return true;
     }
-};
+    return false;
+}
+
+void printBoard(char board[][20]) {
+    for (int i = 0; i < 20; i++) {
+        cout << "  " << i;
+    }
+    cout << endl;
+    for (int y = 0; y < 20; y++) {
+        cout << y;
+        for (int x = 0; x < 20; x++) {
+            cout << " " << board[y][x] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void updateBoard(char board[][20], vector<Ant> ants) {
+    int i, x, y;
+    for (i = 0; i < 10; i++) {
+        x = ants[i].getXCoord();
+        y = ants[i].getYCoord();
+        board[y][x] = 'o';
+    }
+}
+
+vector<int> checkPositions(char board_array[][20], Ant ant) {
+    int x, y;
+    int top_x, top_y;
+    int bottom_x, bottom_y;
+    int left_x, left_y;
+    int right_x, right_y;
+    vector<int> top_coord;
+    vector<int> bottom_coord;
+    vector<int> left_coord;
+    vector<int> right_coord;
+    vector< vector<int> > position_vector;
+
+    x = ant.getXCoord();
+    y = ant.getYCoord();
+    top_x = x;
+    top_y = y - 1;
+    bottom_x = x;
+    bottom_y = y + 1;
+    left_x = x - 1;
+    left_y = y;
+    right_x = x + 1;
+    right_y = y;
+    if (isCellAvailable(board_array, top_x, top_y)) {
+        top_coord.push_back(top_x);
+        top_coord.push_back(top_y);
+        position_vector.push_back(top_coord);
+    }
+    if (isCellAvailable(board_array, bottom_x, bottom_y)) {
+        bottom_coord.push_back(bottom_x);
+        bottom_coord.push_back(bottom_y);
+        position_vector.push_back(bottom_coord);
+    }
+    if (isCellAvailable(board_array, left_x, left_y)) {
+        left_coord.push_back(left_x);
+        left_coord.push_back(left_y);
+        position_vector.push_back(left_coord);
+    }
+    if (isCellAvailable(board_array, right_x, right_y)) {
+        right_coord.push_back(right_x);
+        right_coord.push_back(right_y);
+        position_vector.push_back(right_coord);
+    }
+
+    int position = rand() % position_vector.size();
+    return position_vector[position];
+}
 
 int main() {
     srand(time(nullptr));
 
-    Board board;
+    char board_array[20][20];
+    for (int y = 0; y < 20; y++) {
+        for (int x = 0; x < 20; x++) {
+            board_array[y][x] = '-';
+        }
+    }
 
     int x_ant, y_ant;
     vector<Ant> ant_vector;
@@ -87,39 +175,17 @@ int main() {
         ant_vector.push_back(ant);
     }
 
-    board.updateBoard(ant_vector);
-    board.printBoard();
+    updateBoard(board_array, ant_vector);
+    printBoard(board_array);
 
-    int x, y;
-    int top_x, top_y;
-    int bottom_x, bottom_y;
-    int left_x, left_y;
-    int right_x, right_y;
+    vector<int> newPosition;
     for (int i = 0; i < ant_vector.size(); i++) {
-        x = ant_vector[i].getXCoord();
-        y = ant_vector[i].getYCoord();
-        top_x = x;
-        top_y = y - 1;
-        bottom_x = x;
-        bottom_y = y + 1;
-        left_x = x - 1;
-        left_y = y;
-        right_x = x + 1;
-        right_y = y;
-        if (!board.isCellAvailable(top_x, top_y)) {
-            cout << "ant " << i << ": " << ant_vector[i].getXCoord() << ", " << ant_vector[i].getYCoord() << endl;
-            cout << "top: " << top_x << ", " << top_y << endl;
-        } else if (!board.isCellAvailable(bottom_x, bottom_y)) {
-            cout << "ant " << i << ": " << ant_vector[i].getXCoord() << ", " << ant_vector[i].getYCoord() << endl;
-            cout << "bottom: " << bottom_x << ", " << bottom_y << endl;
-        } else if (!board.isCellAvailable(left_x, left_y)) {
-            cout << "ant " << i << ": " << ant_vector[i].getXCoord() << ", " << ant_vector[i].getYCoord() << endl;
-            cout << "left: " << left_x << ", " << left_y << endl;
-        } else if (!board.isCellAvailable(right_x, right_y)) {
-            cout << "ant " << i << ": " << ant_vector[i].getXCoord() << ", " << ant_vector[i].getYCoord() << endl;
-            cout << "right: " << right_x << ", " << right_y << endl;
-        }
+        newPosition = checkPositions(board_array, ant_vector[i]);
+        ant_vector[i].move(newPosition);
     }
+
+    updateBoard(board_array, ant_vector);
+    printBoard(board_array);
 
     return 0;
 }
