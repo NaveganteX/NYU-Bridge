5ce21d9c2e12f6a50c1b659b1680353c4c8717a0
github username <lhon001@gmail.com>
2020-04-17 00:47:18 -0400

    refactor move member function for predator and prey

--- hw13/hw13_q9/hw13_q9.cpp
+++ hw13/hw13_q9/hw13_q9.cpp
@@ -4,51 +4,7 @@
 #include <vector>
 using namespace std;
 
-class Board {
-    char board[20][20];
-public:
-    Board() {
-        for (int y = 0; y < 20; y++) {
-            for (int x = 0; x < 20; x++) {
-                board[y][x] = '-';
-            }
-        }
-    }
-
-//    void printBoard() {
-//        for (int i = 0; i < 20; i++) {
-//            cout << "  " << i;
-//        }
-//        cout << endl;
-//        for (int y = 0; y < 20; y++) {
-//            cout << y;
-//            for (int x = 0; x < 20; x++) {
-//                cout << " " << board[y][x] << " ";
-//            }
-//            cout << endl;
-//        }
-//    }
-
-//    void updateBoard(vector<Ant> ants) {
-//        int i, x, y;
-//        for (i = 0; i < 10; i++) {
-//            x = ants[i].getXCoord();
-//            y = ants[i].getYCoord();
-//            board[y][x] = 'o';
-//        }
-//    }
-
-//    bool isCellAvailable(int x_coord, int y_coord) {
-//        if (this->getCellStatus(x_coord, y_coord) == '-') {
-//            return true;
-//        }
-//        return false;
-//    }
-
-//    char getCellStatus(int x_coord, int y_coord) {
-//        return board[y_coord][x_coord];
-//    }
-};
+bool isCellAvailable(char board[][20], int x_coord, int y_coord);
 
 class Organism {
     int x, y;
@@ -57,7 +13,7 @@ public:
         x = x_coord;
         y = y_coord;
     }
-    virtual void move(vector<int> position_coords) {};
+    virtual void move(const char board_array[][20]) {};
     int getXCoord() { return x; }
     int getYCoord() { return y; }
     void setXCoord(int x_coord) { x = x_coord; }
@@ -68,10 +24,109 @@ class Ant :public Organism {
 public:
     Ant(int x_coord, int y_coord) :Organism(x_coord, y_coord) {}
 
-    void move(vector<int> position_coords) override {
-        this->setXCoord(position_coords[0]);
-        this->setYCoord(position_coords[1]);
-        Organism::move(position_coords);
+    void move(char board_array[][20]) {
+        int x, y;
+        int top_x, top_y;
+        int bottom_x, bottom_y;
+        int left_x, left_y;
+        int right_x, right_y;
+        vector<int> top_coord;
+        vector<int> bottom_coord;
+        vector<int> left_coord;
+        vector<int> right_coord;
+        vector<vector<int> > position_vector;
+
+        x = this->getXCoord();
+        y = this->getYCoord();
+        top_x = x;
+        top_y = y - 1;
+        bottom_x = x;
+        bottom_y = y + 1;
+        left_x = x - 1;
+        left_y = y;
+        right_x = x + 1;
+        right_y = y;
+        if (isCellAvailable(board_array, top_x, top_y)) {
+            top_coord.push_back(top_x);
+            top_coord.push_back(top_y);
+            position_vector.push_back(top_coord);
+        }
+        if (isCellAvailable(board_array, bottom_x, bottom_y)) {
+            bottom_coord.push_back(bottom_x);
+            bottom_coord.push_back(bottom_y);
+            position_vector.push_back(bottom_coord);
+        }
+        if (isCellAvailable(board_array, left_x, left_y)) {
+            left_coord.push_back(left_x);
+            left_coord.push_back(left_y);
+            position_vector.push_back(left_coord);
+        }
+        if (isCellAvailable(board_array, right_x, right_y)) {
+            right_coord.push_back(right_x);
+            right_coord.push_back(right_y);
+            position_vector.push_back(right_coord);
+        }
+
+        int position = rand() % position_vector.size();
+
+        this->setXCoord(position_vector[position][0]);
+        this->setYCoord(position_vector[position][1]);
+        Organism::move(board_array);
+    }
+};
+
+class Doodlebug :public Organism {
+public:
+    Doodlebug(int x_coord, int y_coord) :Organism(x_coord, y_coord) {}
+
+    void move(char board_array[][20]) {
+        int x, y;
+        int top_x, top_y;
+        int bottom_x, bottom_y;
+        int left_x, left_y;
+        int right_x, right_y;
+        vector<int> top_coord;
+        vector<int> bottom_coord;
+        vector<int> left_coord;
+        vector<int> right_coord;
+        vector<vector<int> > position_vector;
+
+        x = this->getXCoord();
+        y = this->getYCoord();
+        top_x = x;
+        top_y = y - 1;
+        bottom_x = x;
+        bottom_y = y + 1;
+        left_x = x - 1;
+        left_y = y;
+        right_x = x + 1;
+        right_y = y;
+        if (isCellAvailable(board_array, top_x, top_y)) {
+            top_coord.push_back(top_x);
+            top_coord.push_back(top_y);
+            position_vector.push_back(top_coord);
+        }
+        if (isCellAvailable(board_array, bottom_x, bottom_y)) {
+            bottom_coord.push_back(bottom_x);
+            bottom_coord.push_back(bottom_y);
+            position_vector.push_back(bottom_coord);
+        }
+        if (isCellAvailable(board_array, left_x, left_y)) {
+            left_coord.push_back(left_x);
+            left_coord.push_back(left_y);
+            position_vector.push_back(left_coord);
+        }
+        if (isCellAvailable(board_array, right_x, right_y)) {
+            right_coord.push_back(right_x);
+            right_coord.push_back(right_y);
+            position_vector.push_back(right_coord);
+        }
+
+        int position = rand() % position_vector.size();
+
+        this->setXCoord(position_vector[position][0]);
+        this->setYCoord(position_vector[position][1]);
+        Organism::move(board_array);
     }
 };
 
@@ -86,13 +141,32 @@ bool isCellAvailable(char board[][20], int x_coord, int y_coord) {
     return false;
 }
 
-void printBoard(char board[][20]) {
-    for (int i = 0; i < 20; i++) {
-        cout << "  " << i;
-    }
-    cout << endl;
+void initializeBoard(char board[][20], vector<Ant> &ant_vector, vector<Doodlebug> &doodlebug_vector) {
     for (int y = 0; y < 20; y++) {
-        cout << y;
+        for (int x = 0; x < 20; x++) {
+            board[y][x] = '-';
+        }
+    }
+
+    int x_ant, y_ant;
+    for (int i = 0; i < 100; i++) {
+        x_ant = rand() % 20;
+        y_ant = rand() % 20;
+        Ant ant(x_ant, y_ant);
+        ant_vector.push_back(ant);
+    }
+
+    int x_doodlebug, y_doodlebug;
+    for (int i = 0; i < 5; i++) {
+        x_doodlebug = rand() % 20;
+        y_doodlebug = rand() % 20;
+        Doodlebug doodlebug(x_doodlebug, y_doodlebug);
+        doodlebug_vector.push_back(doodlebug);
+    }
+}
+
+void printBoard(char board[][20]) {
+    for (int y = 0; y < 20; y++) {
         for (int x = 0; x < 20; x++) {
             cout << " " << board[y][x] << " ";
         }
@@ -100,92 +174,51 @@ void printBoard(char board[][20]) {
     }
 }
 
-void updateBoard(char board[][20], vector<Ant> ants) {
+void updateBoard(char board[][20], vector<Ant> ants, vector<Doodlebug> doodlebug) {
+    for (int y = 0; y < 20; y++) {
+        for (int x = 0; x < 20; x++) {
+            board[y][x] = '-';
+        }
+    }
+
     int i, x, y;
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < ants.size(); i++) {
         x = ants[i].getXCoord();
         y = ants[i].getYCoord();
         board[y][x] = 'o';
     }
-}
 
-vector<int> checkPositions(char board_array[][20], Ant ant) {
-    int x, y;
-    int top_x, top_y;
-    int bottom_x, bottom_y;
-    int left_x, left_y;
-    int right_x, right_y;
-    vector<int> top_coord;
-    vector<int> bottom_coord;
-    vector<int> left_coord;
-    vector<int> right_coord;
-    vector< vector<int> > position_vector;
-
-    x = ant.getXCoord();
-    y = ant.getYCoord();
-    top_x = x;
-    top_y = y - 1;
-    bottom_x = x;
-    bottom_y = y + 1;
-    left_x = x - 1;
-    left_y = y;
-    right_x = x + 1;
-    right_y = y;
-    if (isCellAvailable(board_array, top_x, top_y)) {
-        top_coord.push_back(top_x);
-        top_coord.push_back(top_y);
-        position_vector.push_back(top_coord);
+    for (i = 0; i < doodlebug.size(); i++) {
+        x = doodlebug[i].getXCoord();
+        y = doodlebug[i].getYCoord();
+        board[y][x] = 'X';
     }
-    if (isCellAvailable(board_array, bottom_x, bottom_y)) {
-        bottom_coord.push_back(bottom_x);
-        bottom_coord.push_back(bottom_y);
-        position_vector.push_back(bottom_coord);
-    }
-    if (isCellAvailable(board_array, left_x, left_y)) {
-        left_coord.push_back(left_x);
-        left_coord.push_back(left_y);
-        position_vector.push_back(left_coord);
-    }
-    if (isCellAvailable(board_array, right_x, right_y)) {
-        right_coord.push_back(right_x);
-        right_coord.push_back(right_y);
-        position_vector.push_back(right_coord);
-    }
-
-    int position = rand() % position_vector.size();
-    return position_vector[position];
 }
 
 int main() {
     srand(time(nullptr));
-
+    int time_step = 0;
     char board_array[20][20];
-    for (int y = 0; y < 20; y++) {
-        for (int x = 0; x < 20; x++) {
-            board_array[y][x] = '-';
-        }
-    }
-
-    int x_ant, y_ant;
     vector<Ant> ant_vector;
-    for (int i = 0; i < 10; i++) {
-        x_ant = rand() % 20;
-        y_ant = rand() % 20;
-        Ant ant(x_ant, y_ant);
-        ant_vector.push_back(ant);
-    }
+    vector<Doodlebug> doodlebug_vector;
+    initializeBoard(board_array, ant_vector, doodlebug_vector);
 
-    updateBoard(board_array, ant_vector);
-    printBoard(board_array);
+    do {
+        cout << "Time step: " << time_step << endl;
+        updateBoard(board_array, ant_vector, doodlebug_vector);
+        printBoard(board_array);
 
-    vector<int> newPosition;
-    for (int i = 0; i < ant_vector.size(); i++) {
-        newPosition = checkPositions(board_array, ant_vector[i]);
-        ant_vector[i].move(newPosition);
-    }
-
-    updateBoard(board_array, ant_vector);
-    printBoard(board_array);
+//    exit code 8 happens somewhere in this for loop; suspect a floating point exception
+        for (int i = 0; i < doodlebug_vector.size(); i++) {
+            doodlebug_vector[i].move(board_array);
+        }
+        for (int i = 0; i < ant_vector.size(); i++) {
+            ant_vector[i].move(board_array);
+        }
+        cout << "Press [Enter] to advance 1 time step.";
+        cout << endl;
+        time_step++;
+    } while (cin.get());
 
     return 0;
 }
