b383b4aef1f72f4a5a67a8d5b77e6fa6a9b1a99a
github username <lhon001@gmail.com>
2020-04-20 20:51:33 -0400

    runtime analysis of selection sort algorithm

--- search_sort/sort/sort.cpp
+++ search_sort/sort/sort.cpp
@@ -15,7 +15,14 @@ void insertionSort(vector<T> &a) {
     }
 }
 
-int find_index_of_min(int arr[], int low, int index_of_high) {
+/*
+ * findIndexOfMin() is a helper function for selectionSort().
+ * if we let n = high - (low + 1) then the runtime of findIndexOfMin() is theta(n)
+ * because at the worst the function will iterate over the array_size - 1. we do not include
+ * the first element of the input array because we are comparing it to the next element up to the last element.
+ * Therefore f(n) = theta(n) for the findIndexOfMin() helper function.
+ */
+int findIndexOfMin(int arr[], int low, int index_of_high) {
     int min = arr[low], min_index = low;
     for (int i = low + 1; i <= index_of_high; i++) {
         if (arr[i] < min) {
@@ -26,11 +33,21 @@ int find_index_of_min(int arr[], int low, int index_of_high) {
     return min_index;
 }
 
+/*
+ * again we define n as the size of the input array; n = high - (low + 1)
+ * for each iteration the size of the input array decreases like so (i.e the "cost" of the for loop
+ * and calling findIndexOfMin() is (n - i). so n, then n-1, n-2, etc)
+ * n + (n-1) + (n-2) + (n-3) + ... + 2 + 1 = theta(n^2)
+ * 
+ * Thus, the selectionSort() algorithm (which includes the findIndexOfMin() helper function) has a
+ * quadratic runtime (n^2)
+ */
+
 void selectionSort(int *arr, int arr_size) {
     int min_index;
     for (int i = 0; i < arr_size; i++) {
         int temp;
-        min_index = find_index_of_min(arr, i, arr_size - 1);
+        min_index = findIndexOfMin(arr, i, arr_size - 1);
         temp = arr[i];
         arr[i] = arr[min_index];
         arr[min_index] = temp;
