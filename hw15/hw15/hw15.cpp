68a0556d72a6cd694b90c9a2d10352d0a32d858e
github username <lhon001@gmail.com>
2020-04-28 23:16:52 -0400

    wip implement Linked List class

--- hw15/hw15/hw15.cpp
+++ hw15/hw15/hw15.cpp
@@ -4,11 +4,20 @@
 #include <vector>
 using namespace std;
 
+template <class T>
+class LListNode {
+    T data;
+    LListNode<T> *next;
+
+public:
+    explicit LListNode(T new_data = T(), LListNode<T> *new_next = nullptr) : data(new_data), next(new_next)
+};
+
 class Employee {
     string name;
     int id_num;
-    double hourly_pay_rate;
     int hours_worked;
+    double hourly_pay_rate;
 
 public:
     Employee(string input_name, int input_id_num, double input_hourly_pay_rate) :
@@ -21,10 +30,10 @@ public:
     double getHourlyPayRate() { return hourly_pay_rate; }
     int getHoursWorked() { return hours_worked; }
     void setHoursWorked(int input_hours_worked) { hours_worked = input_hours_worked; }
+    double getTotalPay() {
+        return hours_worked * hourly_pay_rate;
+    }
 
-//    istream& operator >>(istream& ins, Employee& id) {
-//
-//    }
 };
 
 void openInputFile(ifstream &inFile) {
@@ -44,14 +53,37 @@ void openInputFile(ifstream &inFile) {
 
 int main() {
     vector<Employee> employee_vector; // to be converted to linked list later
+
     ifstream employee_data_file;
     openInputFile(employee_data_file);
+
     int temp_id;
     double temp_hourly_rate;
     string temp_name;
 
-    while (employee_data_file >> temp_name) {
-        cout << temp_name << " " << endl;
+    while (employee_data_file >> temp_id) {
+        employee_data_file >> temp_hourly_rate;
+        employee_data_file.ignore(9999, '\t');
+        getline(employee_data_file, temp_name);
+
+        Employee temp_employee(temp_name, temp_id, temp_hourly_rate);
+        employee_vector.push_back(temp_employee);
+    }
+
+    ifstream employee_payroll_file;
+    openInputFile(employee_payroll_file);
+
+    int temp_hours_worked;
+    while (employee_payroll_file >> temp_id) {
+        employee_payroll_file >> temp_hours_worked;
+        // loop through linked list and find the employee with matching ID and assign temp_hours_worked
+    }
+
+    for (int i = 0; i < employee_vector.size(); ++i) {
+        cout << employee_vector[i].getEmployeeID() << endl;
+        cout << employee_vector[i].getHourlyPayRate() << endl;
+        cout << employee_vector[i].getEmployeeName() << endl;
+        cout << endl;
     }
 
     return 0;
