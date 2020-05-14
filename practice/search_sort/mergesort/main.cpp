9fc56ba072104a3b562e8feb4f583a9722fd1c24
github username <lhon001@gmail.com>
2020-05-14 10:19:44 -0400

    mergesort practice

--- practice/search_sort/mergesort/main.cpp
+++ practice/search_sort/mergesort/main.cpp
@@ -1,37 +1,56 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 template <class T>
-void merge(vector<T> &arr, int left, int middle, int right) {
-    vector<T> temp_left;
+void merge(vector<T> &arr, vector<T> &temp_arr, int left_pos, int right_pos, int right_end) {
+    int left_end = right_pos - 1;
+    int temp_pos = left_pos;
+    int num_elements = right_end - left_pos + 1;
+    while (left_pos <= left_end && right_pos <= right_end) {
+        if (arr[left_pos] <= arr[right_pos]) {
+            temp_arr[temp_pos++] = arr[left_pos++];
+        } else {
+            temp_arr[temp_pos++] = arr[right_pos++];
+        }
+    }
 
+    while (left_pos <= left_end) {
+        temp_arr[temp_pos++] = arr[left_pos++];
+    }
+
+    while(right_pos <= right_end) {
+        temp_arr[temp_pos++] = arr[right_pos++];
+    }
+
+    for (int i = 0; i < num_elements; i++, right_end--) {
+        arr[right_end] = temp_arr[right_end];
+    }
 }
 
 template <class T>
-void mergeSort(vector<T> &arr, int left, int right) {
-    if (arr.size() == 1) {
-        return arr;
-    } else if (arr.size() == 2) {
-        if (arr[0] > arr[1]) {
-            T temp = arr[0];
-            arr[0] = arr[1];
-            arr[1] = temp;
-        }
-        return arr;
+void mergeSort(vector<T> &arr, vector<T> &temp_arr, int left, int right) {
+    if (left < right) {
+        int center = (left + right) / 2;
+        mergeSort(arr, temp_arr, left, center);
+        mergeSort(arr, temp_arr, center + 1, right);
+        merge(arr, temp_arr, left, center + 1, right);
     }
+}
 
-    // split arr into two (a left and right subarray)
-    // recursively call mergesort on those subarrays
-    int middle = (left + right) / 2;
-    mergeSort(arr, left, middle);
-    mergeSort(arr, middle + 1, right);
-    merge(arr, left, middle, right);
+template <class T>
+void mergeSort(vector<T> &arr) {
+    vector<T> temp_arr(arr.size());
+    mergeSort(arr, temp_arr, 0, arr.size() - 1);
 }
 
 int main() {
+    vector<int> arr = {6, 6, 7, 1, 3, 100 , 50, 34};
+
+    mergeSort(arr);
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
-
-// test cases: size 0, 1, 2, 3
-
